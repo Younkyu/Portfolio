c34bf2e54fd7f8fbf48eeccccff0dcacfa778515
Younkyu <younkyu2004@gmail.com>
2022-09-23 14:23:15 -0700

    testing pathing 6

--- src/components/NavigationBar/NavigationBar.js
+++ src/components/NavigationBar/NavigationBar.js
@@ -21,7 +21,7 @@ function NavigationBar() {
                             <Nav.Link href="./#/about">About</Nav.Link>
                             <Nav.Link href="./#/skills">Skills</Nav.Link>
                             <Nav.Link href="./#/projects">Projects</Nav.Link>
-                            <Nav.Link href="./contact">Contact</Nav.Link>
+                            <Nav.Link href="./#/contact">Contact</Nav.Link>
                         </Nav>
                     </Navbar.Collapse>
                 </Container>
