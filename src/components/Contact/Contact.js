70b7aa8a2427f871d46045fb4144608c278885d3
Younkyu <younkyu2004@gmail.com>
2022-09-24 05:00:29 -0700

    added modal to contact page

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,12 +1,21 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
+import Modal from 'react-bootstrap/Modal';
+import Button from 'react-bootstrap/Button';
 import emailjs from '@emailjs/browser';
 import './Contact.css';
 
 function Contact() {
+    const [show, setShow] = useState(false);
+
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+
     const form = useRef();
 
     const sendEmail = (e) => {
         e.preventDefault();
+
+        handleShow();
 
         emailjs.sendForm('service_6uhqknf', 'template_7c4ijjn', form.current, 'ibW3v1409oUCBJEmE')
             .then((result) => {
@@ -21,6 +30,14 @@ function Contact() {
             <div id='contact-page' className='box-container center-content'>
                 <h2>Contact Me</h2>
                 <div id='contact-content-box' className='horizontal-flex-box'>
+
+                    <Modal show={show} onHide={handleClose}>
+                        <Modal.Header closeButton>
+                            <Modal.Title>Message Sent!</Modal.Title>
+                        </Modal.Header>
+                        <Modal.Body>Thank you for your message, I will try to get back to you soon.</Modal.Body>
+                    </Modal>
+
                     <img src={require("../../images/contact.png")} alt="contact image" id="contact-image" />
                     <form className='vertical-flex-box' ref={form} onSubmit={sendEmail}>
                         <label>Name</label>
